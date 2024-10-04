de118bc3f3cd9bb676506d90062dc18aeefefb24
Blair Replogle <UpBra@users.noreply.github.com>
2024-10-04 14:57:28 -0600

    TLK-0007: Add Ruby Additions (#7)

---
+++ lib/fastlane/plugin/toolkit/source/ruby.rb
@@ -0,0 +1,17 @@
+# -------------------------------------------------------------------------
+#
+# Ruby Additions
+#
+# -------------------------------------------------------------------------
+
+# String
+
+class ::String
+
+	def tail(n = 15)
+		value = self.clone
+		lines = value.lines
+		start = [ lines.count - n, 0 ].max
+		lines[start..-1].join
+	end
+end
--- lib/fastlane/plugin/toolkit/version.rb
+++ lib/fastlane/plugin/toolkit/version.rb
@@ -7,6 +7,6 @@
 module Fastlane
 
 	module Toolkit
-		VERSION = "0.1.5"
+		VERSION = "0.1.6"
 	end
 end
